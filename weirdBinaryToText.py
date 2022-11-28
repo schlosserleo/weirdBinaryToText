bc7e36fcb1af0cf50180af122f970a9b1bbf7945
schlosserleo <gymnasium.ottweiler@gmail.com>
2022-11-28 17:25:40 +0100

    Final Version of the code(2)

--- weirdBinaryToText.py
+++ weirdBinaryToText.py
@@ -7,7 +7,17 @@ text_param = ""
 selected_lang = ""
 weird_param = ""
 lang_dict = {"EN": ["Zero", "One"], "DE": ["Null", "Eins"], "FR": ["Zéro", "Un"], "ES": ["Cero", "Uno"]}
-help_text = ""  # TODO: Write a Help / Man Page
+help_text = " Version 1.0 of The Weird Binary Converter\n" \
+            "------------------------------------------------------------------------------\n" \
+            "\033[4mFormats\033[0m: w = weird binary. e.g.: 'ZeroOneZero...\n" \
+            "         You have to declare a language for this format. e.g.: '-l EN'\n" \
+            "         b = binary e.g.: '01100001'\n" \
+            "         t = normal text e.g.: 'abcdefg\n\n" \
+            "\033[4mModes\033[0m:   bw: binary => weird binary.\n         wb: weird binary => binary.\n         bt: " \
+            "binary => normal text.\n         tb: normal text => binary.\n         wt: weird binary => normal text.\n" \
+            "         tw: normal text => weird binary\n\n\033[4mSyntax\033[0m:\n" \
+            "python weirdBinaryToText.py -[input format] <input> (-l <EN/DE/...>) -[mode]\n\n" \
+            "\033[4mExample\033[0m:\npython weirdBinaryToText.py -t ”Hello World” -l EN -tw"
 
 
 def bin_to_weird(convert_param, lang):
@@ -66,7 +76,7 @@ while sys.argv:
         mode = "tw"
 
     elif re.search(r'^(-h)$', arg):
-        print(help_text)
+        mode = "help"
 
 if mode == "bw":
     print(bin_to_weird(bin_param, selected_lang))
@@ -80,5 +90,7 @@ elif mode == "wt":
     print(bin_to_text(weird_to_bin(weird_param, selected_lang)))
 elif mode == "tw":
     print(bin_to_weird(text_to_bin(text_param), selected_lang))
+elif mode == "help":
+    print(help_text)
 else:
     print("You have to give an argument. e.g.:\n\n'python example.py -b 001011010 0100100110 -l EN -bttb'")
